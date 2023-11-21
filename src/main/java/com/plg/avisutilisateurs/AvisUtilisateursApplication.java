4520808306cb2d85d7b9413322c6108578c34756
Gaspard PETEMOYA <gpetemoya@rawsur.com>
2023-11-21 10:07:26 +0100

    first commit

---
+++ src/main/java/com/plg/avisutilisateurs/AvisUtilisateursApplication.java
@@ -0,0 +1,22 @@
+package com.plg.avisutilisateurs;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class AvisUtilisateursApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisUtilisateursApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Serveur run");
+	}
+
+}
---
+++ src/test/java/com/plg/avisutilisateurs/AvisUtilisateursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plg.avisutilisateurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvisUtilisateursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
